73a01f901cc7e8ae8c41ecd2b200f1dad41684d7
ywq <yewq1@lenovo.com>
2021-05-05 05:44:56 +0800

    first construct

---
+++ FollowVehicleFPS/FollowVehicleFPS.cs
@@ -0,0 +1,15 @@
+﻿using ICities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FollowVehicleFPS
+{
+    public class FollowVehicleFPS : IUserMod
+    {
+        public String Name => "FollowVehicleFPS";
+        public String Description => "Following Vehicle FPS";
+    }
+}
