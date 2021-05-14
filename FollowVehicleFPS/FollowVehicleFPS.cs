88a0eba78d43503b152833aca604618e5db29ede
oldevil1974 <oldevil@139.com>
2021-05-14 19:21:26 +0800

    +can follow

--- FollowVehicleFPS/FollowVehicleFPS.cs
+++ FollowVehicleFPS/FollowVehicleFPS.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace FollowVehicleFPS
 {
@@ -11,5 +12,19 @@ namespace FollowVehicleFPS
     {
         public String Name => "FollowVehicleFPS";
         public String Description => "Following Vehicle FPS";
+    }
+
+    public class ModLoad : LoadingExtensionBase
+    {
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+            Debug.Log("FollowVehicleFPS Mod OnLevelLoaded");
+            var controller = GameObject.FindObjectOfType<CameraController>();
+            controller.gameObject.AddComponent<VehicleCamera>();
+        }
+        public override void OnLevelUnloading()
+        {
+            Debug.Log("FollowVehicleFPS Mod OnLevelUnloading");
+        }
     }
 }
---
+++ FollowVehicleFPS/VehicleCamera.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace FollowVehicleFPS
+{
+    class VehicleCamera : MonoBehaviour
+    {
+        public bool following = false;
+        private ushort followingVehicleId;
+        private Camera camera;
+        private VehicleManager vehicleMgr;
+
+        void Awake()
+        {
+            camera = GetComponent<Camera>();
+            vehicleMgr = VehicleManager.instance;
+        }
+        void Start()
+        {
+            Debug.Log("VehicleCamera Start");
+        }
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F10))
+            {
+                if (following)
+                {
+                    StopFollowing();
+                }
+                else
+                {
+                    ushort vehicleid = GetRandomVehicle();
+                    if (vehicleid != 0 )
+                        StartFollowing(vehicleid);
+                }
+            }
+        }
+        void LateUpdate()
+        {
+            if (following)
+            {
+                Vehicle vehicle = vehicleMgr.m_vehicles.m_buffer[followingVehicleId];
+                Vector3 pos;
+                Quaternion oritation;
+                vehicle.GetSmoothPosition(followingVehicleId, out pos, out oritation);
+                camera.transform.position = pos;
+                camera.transform.rotation = oritation;
+            }
+        }
+
+        public void StartFollowing(ushort vehicleid) {
+            following = true;
+            followingVehicleId = vehicleid;
+            Debug.Log("StartFollowing " + vehicleid);
+        }
+        public void StopFollowing() {
+            following = false;
+            Debug.Log("StopFollowing ");
+        }
+
+        private ushort GetRandomVehicle()
+        {
+            for(ushort i = 0; i < vehicleMgr.m_vehicleCount - 1; i++)
+            {
+                Vehicle vehicle = vehicleMgr.m_vehicles.m_buffer[i];
+                if ((vehicle.m_flags & (Vehicle.Flags.Created | Vehicle.Flags.Deleted))!= Vehicle.Flags.Created)
+                {
+                    continue;
+                }
+                if(vehicle.Info.m_vehicleAI is CarTrailerAI)
+                {
+                    continue;
+                }
+                return i;
+            }
+            return 0;
+        }
+    }
+}
